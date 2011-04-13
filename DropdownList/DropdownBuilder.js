f3cdc9b4d49ef4196c9ed7cdf3073f15b74c82de
dashersw <armagan@tart.com.tr>
2011-04-13 06:33:36 +0300

    Added the DropdownList and respective Builder classes.

---
+++ DropdownList/DropdownBuilder.js
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) 2011 Techinox Information Technologies (http://www.techinox.com)
+ * Techinox Commercial License
+ *
+ * @author armagan.amcalar@tart.com.tr (Armagan Amcalar)
+ */
+
+/**
+ * @fileoverview Builder class for dropdown lists.
+ */
+goog.require('tart.Builder');
+goog.provide('tart.DropdownBuilder');
+
+
+
+/**
+ * DropdownBuilder class builds the DOM for DropdownList class.
+ *
+ * @constructor
+ * @extends {tart.Builder}
+ * @param {string} id id for the builder. Can be used as the id of the DOM element this builder will build.
+ * @return {tart.DropdownBuilder} A builder object.
+ */
+tart.DropdownBuilder = function(id) {
+    goog.base(this, id);
+    this.owner = null;
+    return this;
+};
+goog.inherits(tart.DropdownBuilder, tart.Builder);
+
+
+/**
+ * Main namespace of HTML templates for DOM structure.
+ */
+tart.DropdownBuilder.templates = {};
+
+
+/**
+ * Select menu template.
+ * @param {string} id Id of the container.
+ * @param {string} optionListHTML HTML of the list of options.
+ * @return {string} the container HTML.
+ */
+tart.DropdownBuilder.templates.container = function(id, optionListHTML) {
+    return '<select id="' + id + '">' + optionListHTML + '</select>';
+};
+
+
+/**
+ * Generates a select menu element, converts it to a jQuery object and passes to this.dom_.
+ *
+ * @param {tart.List} owner Owner tart.List instance.
+ */
+tart.DropdownBuilder.prototype.buildDOM = function(owner) {
+    this.owner = owner;
+    var opts = [];
+    for (var k = 0, z = this.owner.keys_.length; k < z; k++) {
+        var option = tart.DropdownBuilder.templates.option(
+            this.owner.keys_[k], this.owner.values_[k], (k == this.owner.getActiveItemIndex()));
+        opts.push(option);
+    }
+    var finalDOM = tart.DropdownBuilder.templates.container(this.id_, opts.join(''));
+    this.dom_ = $(finalDOM);
+    this.dom_.change(function() {
+        owner.switchIndex($(this).attr('selectedIndex'));
+    });
+};
+
+
+/**
+ * Generates a single option element which ready-to-use in a HTML select menu element.
+ *
+ * @param {string|number} key Option value.
+ * @param {string|number} value Option value.
+ * @param {boolean} selected Whether the option is selected.
+ * @return {string} the option HTML.
+ */
+tart.DropdownBuilder.templates.option = function(key, value, selected) {
+    var active = (selected) ? ' selected="selected"' : '';
+    return '<option value="' + key + '"' + active + '>' + value + '</option>';
+};
+
+
+/**
+ * Sets an select option's "selected" property to True.
+ *
+ * @param {number} newIndex the new index of the element to set the dropdown list to.
+ */
+tart.DropdownBuilder.prototype.changeActiveItem = function(newIndex) {
+    this.dom_.attr('selectedIndex', newIndex).change();
+};
+
+
+/**
+ * Removes a single select option from DOM by index which given as parameter.
+ *
+ * @param {number} index the index of the element to remove from the list.
+ */
+tart.DropdownBuilder.prototype.removeOption = function(index) {
+    this.dom_.children().eq(index).remove();
+};
+
+
+/**
+ * Creates a single select option and attachs it to the current DOM.
+ *
+ * @param {string|number} key value for the option.
+ * @param {string|number} value Text for the option.
+ * @return {jQueryObject} the jQuery object that holds the DOM object of the dropdown list.
+ */
+tart.DropdownBuilder.prototype.addOption = function(key, value) {
+    return this.$dom_.append(tart.DropdownBuilder.templates.option(key, value, false));
+};
+
+
+/**
+ * Removes select menu from DOM with events.
+ */
+tart.DropdownBuilder.prototype.removeDOM = function() {
+    this.$dom_.remove();
+};
---
+++ DropdownList/DropdownList.js
@@ -0,0 +1,112 @@
+/**
+ * Copyright (c) 2011 Techinox Information Technologies (http://www.techinox.com)
+ * Techinox Commercial License
+ *
+ * @author armagan.amcalar@tart.com.tr (Armagan Amcalar)
+ */
+
+/**
+ * @fileoverview Dropdown list class is an example of an HTML select box.
+ *
+ * Example usage:
+ * var items        = {key1:'val1',key2:'val2',key3:'val3'};
+ * var builder      = new tart.DropdownBuilder('elementId');
+ * var selectedItem = 1;
+ * var list = new tart.DropdownList(items, builder, selectedItem);
+ * list.getAll(); // Outputs [{key1:'val1'},{key2:'val2'},{key3:'val3'}]
+ * list.getDOM(); // Returns a select (dropdown) menu in jquery format jQuery(select#elementId)
+ * list.setSelected(2); // Sets key3:val3 option element selected property to TRUE;
+ * list.getActiveItem(); // Returns an Object { key3="val3" }
+ */
+
+goog.provide('tart.DropdownList');
+goog.require('tart.DropdownBuilder');
+goog.require('tart.List');
+
+
+
+/**
+ * Constructor method for DropdownList.
+ *
+ * @constructor
+ * @extends {tart.List}
+ * @param {Object|Array}  initialList    initial list of items.
+ * @param {tart.Builder=} opt_builder    builder class.
+ * @param {number=}       opt_activeItem index of the active item.
+ */
+tart.DropdownList = function(initialList, opt_builder, opt_activeItem) {
+    goog.base(this, initialList, opt_activeItem);
+    this.builder = opt_builder || new tart.DropdownBuilder('');
+    this.builder.buildDOM(this);
+};
+goog.inherits(tart.DropdownList, tart.List);
+
+
+/**
+ * @inheritDoc
+ */
+tart.DropdownList.prototype.removeByIndex = function(index) {
+    var result = this.constructor.superClass_.removeByIndex.call(this, index);
+    if (result === false) {
+        return -1;
+    } else if (result === -1) {
+        this.setActiveItemIndex(0);
+        return 0;
+    } else {
+        if (this.builder) {
+            this.builder.removeOption(result);
+        }
+        return result;
+    }
+};
+
+
+/**
+ * @inheritDoc
+ */
+tart.DropdownList.prototype.removeDOM = function() {
+    return this.builder.removeDOM();
+};
+
+
+/**
+ * @inheritDoc
+ */
+tart.DropdownList.prototype.getDOM = function() {
+    return this.builder.getDOM();
+};
+
+
+/**
+ * @inheritDoc
+ */
+tart.DropdownList.prototype.setActiveItemIndex = function(newIndex) {
+    this.switchIndex(newIndex);
+    if (this.builder) {
+        this.builder.changeActiveItem(this.getActiveItemIndex());
+    }
+};
+
+/**
+ * Triggered by this.builder.dom_ element when user change the index by non-programatically way.
+ * Important: This method shouldn't be called in any implementation code. Developers should use
+ *            this.setActiveItemIndex() method instead of this.
+ *
+ * @param {number} newIndex index of the item to set as active.
+ */
+
+tart.DropdownList.prototype.switchIndex = function(newIndex) {
+    this.constructor.superClass_.setActiveItemIndex.call(this, ((newIndex < 0) ? 0 : newIndex));
+};
+
+
+/**
+ * @inheritDoc
+ */
+tart.DropdownList.prototype.addItem = function(key, value) {
+    var added = this.constructor.superClass_.addItem.call(this, key, value);
+    if (this.builder && added === true) {
+        this.builder.addOption(key, value);
+    }
+    return this;
+};
