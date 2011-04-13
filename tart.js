54448e424bebe3059ad5fb30281644ae7d11398a
dashersw <armagan@tart.com.tr>
2011-04-13 06:03:34 +0300

    Added the tart.js base file.

---
+++ tart.js
@@ -0,0 +1,17 @@
+/**
+ * Techinox Javascript Library base
+ *
+ * Copyright (c) 2009-2010 Techinox Information Technologies (http://www.techinox.com)
+ * Techinox Commercial License
+ *
+ * @author armagan.amcalar@tart.com.tr (Armagan Amcalar)
+*/
+
+/**
+ * @fileoverview tartJS base.
+ */
+goog.provide('tart');
+
+
+/** @typedef {Object} */
+tart.JSON;
