e7d6a8186d61c68e6efc567b6bb4374bf3d881b1
Osman Yuksel <osman.yuksel@tart.com.tr>
2011-04-18 16:04:08 +0300

    Code linting and commenting

--- Validator/Validator.js
+++ Validator/Validator.js
@@ -1,7 +1,19 @@
-var Validator = function () {};
+var Validator = function() {
+};
 
+/**
+ * Validator which looks for given text's format
+ * like is email, is phoneNumber etc
+ */
 Validator.prototype.is = {};
-Validator.prototype.is.email = function (text) {
+
+/**
+ * Checks if given text is valid email
+ *
+ * @param {string} text email text to be validated.
+ * @return {bool} true if its valid email.
+ */
+Validator.prototype.is.email = function(text) {
     var pattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     return pattern.test(text);
 };
