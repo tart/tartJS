2e9401749b138b1dc29d818af73ab2f2ca4171a3
Osman Yuksel <osman.yuksel@tart.com.tr>
2011-04-19 15:09:54 +0300

    GJslint fixes for Validator.js

--- Validator/Validator.js
+++ Validator/Validator.js
@@ -1,9 +1,26 @@
-goog.provide("tart.Validator");
-goog.require("tart.Validator_");
+goog.provide('tart.Validator');
+goog.require('tart.Validator_');
 
 
+
+/**
+ * Tart validator to check if string or number is valid for a specified format
+ *
+ * @constructor
+ */
 tart.Validator = function() {
 };
 
+/**
+ * @type {Object}
+ *
+ * Validator methods, which related to check for input's format
+ */
 tart.Validator.prototype.is = tart.Validator_.is;
+
+/**
+ * @type {Object}
+ *
+ * Validator methods, which related to check for input's attributes ie. char length
+ */
 tart.Validator.prototype.has = tart.Validator_.has;
