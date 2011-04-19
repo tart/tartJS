8d797e6b6c3b3b680135640ee527615eadb46d57
Osman Yuksel <osman.yuksel@tart.com.tr>
2011-04-19 15:20:27 +0300

    GJslint fixes for Validator_.is[2~

--- Validator/Validator_.is.js
+++ Validator/Validator_.is.js
@@ -1,4 +1,4 @@
-goog.provide("tart.Validator_.is");
+goog.provide('tart.Validator_.is');
 
 /**
  * Checks if given text is valid email
@@ -12,18 +12,36 @@ tart.Validator_.is.email = function(text) {
 };
 
 
+/**
+ * Checks if given text doesnt contains only white spaces but some chars or numbers
+ *
+ * @param {string} text text to be validated.
+ * @return {bool} true if text contains any char or number.
+ */
 tart.Validator_.is.notOnlySpace = function(text) {
     var result = $.trim(text).length > 0;
     return result;
 };
 
 
+/**
+ * Checks if given text contains only numeric chars
+ *
+ * @param {string} text text to be validated.
+ * @return {bool} true if text contains only numbers.
+ */
 tart.Validator_.is.numeric = function(text) {
     var pattern = /^[0-9]+$/;
     return pattern.test(text);
 };
 
 
+/**
+ * Checks if given text contains both digit and non-digit chars
+ *
+ * @param {string} text text to be validated.
+ * @return {bool} true if text contains both digit and non-digit chars.
+ */
 tart.Validator_.is.digitAndNonDigit = function(text) {
     var pattern = /(\d\D)|(\D\d)/;
     return pattern.test(text);
