d7590750d9a2ab121cd9fbf52978c369ea04aa1a
Osman Yuksel <osman.yuksel@tart.com.tr>
2011-04-19 15:16:51 +0300

    GJslint fixes for Validator_.has

--- Validator/Validator_.has.js
+++ Validator/Validator_.has.js
@@ -1,20 +1,47 @@
-goog.provide("tart.Validator_.has");
+goog.provide('tart.Validator_.has');
 
 
+/**
+ * Checks for strings' min length
+ *
+ * @param {string} text string to check for char length.
+ * @param {number} value char length value.
+ * @return {bool} true if string's length > value.
+ */
 tart.Validator_.has.minLength = function(text, value) {
     return (text.length >= value);
 };
 
-
+/**
+ * Checks for string's max length
+ *
+ * @param {string} text string to check for char length.
+ * @param {number} value char length value.
+ * @return {bool} true if string's length < value.
+ */
 tart.Validator_.has.maxLength = function(text, value) {
     return (text.length <= value);
 };
 
 
+/**
+ * Checks for string or number's min numeric value
+ *
+ * @param {string|number} num number to check value for.
+ * @param {number} value value to check.
+ * @return {bool} true if string's num < value.
+ */
 tart.Validator_.has.minValue = function(num, value) {
     return num >= value;
 };
 
+/**
+ * Checks for string or number's max numeric value
+ *
+ * @param {string|number} num number to check value for.
+ * @param {number} value value to check.
+ * @return {bool} true if string's num > value.
+ */
 tart.Validator_.has.maxValue = function(num, value) {
     return num <= value;
 };
