235cefc85057f6d2d5743a2a14f89f83b6e44d2f
Osman Yuksel <osman.yuksel@tart.com.tr>
2011-05-18 15:39:53 +0300

    [TartBox] spec which checks for setBodyForEl cases succeded

--- tart/TartBox/TartBox.js
+++ tart/TartBox/TartBox.js
@@ -13,9 +13,19 @@
 // limitations under the License.
 goog.provide("tart.TartBox");
 
+/**
+ * @param {string|jQuery} body 
+ */
 tart.TartBox.init = function (title, body, type) {
     tart.TartBox.title = title;
     tart.TartBox.body = body;
     tart.TartBox.type = type;
 };
 
+tart.TartBox.setBodyFromEl = function (el) {
+    if (!el || !el.jquery || el.length == 0) {
+        throw new Error("Element not exists");
+    }
+
+    tart.TartBox.body = el.html();
+};
--- tart/TartBox/spec/TartBoxSpec.js
+++ tart/TartBox/spec/TartBoxSpec.js
@@ -44,10 +44,39 @@ describe("TartBox", function () {
             tart.TartBox.init.call(dummyObject, params.title, params.body, params.type);
         });
 
-
         it("should construct with a title again", function () {
             expect(tart.TartBox.title).toEqual(params.title);
         });
 
     });
+
+    describe("body can be set from a DOM element's content", function () {
+        var dummyEl = $("<div>").html("this is dummy text");
+
+        beforeEach(function () {
+            tart.TartBox.init();
+        });
+
+        it("should set tartBox's body attribute to elements content if element exists", function () {
+           tart.TartBox.setBodyFromEl(dummyEl);
+           expect(tart.TartBox.body).toEqual("this is dummy text");
+        });
+
+        it("should throw an exception if element not exists", function () {
+           var elementNotExists;
+           expect(function () {tart.TartBox.setBodyFromEl(elementNotExists)}).toThrow("Element not exists");
+        });
+
+        it("should throw an exception if element not a jQuery object exists", function () {
+           var elementNotExists = "foobar";
+           expect(function () {tart.TartBox.setBodyFromEl(elementNotExists)}).toThrow("Element not exists");
+        });
+
+        it("should throw an exception if element not a jQuery object which contains a DOM element", function () {
+           var elementNotExists = $(".whichIsNotExists");
+           expect(function () {tart.TartBox.setBodyFromEl(elementNotExists)}).toThrow("Element not exists");
+        });
+
+    });
+
 });
