f2776f447e02b34d49a87a756e45c42f7cbc27e8
Osman Yuksel <osman.yuksel@tart.com.tr>
2011-05-27 11:21:11 +0300

    [component][model] Added a spec to check subClass' event driven architecture

--- tart/components/spec/ModelSpec.js
+++ tart/components/spec/ModelSpec.js
@@ -1,8 +1,29 @@
 describe('Component Model', function() {
+
     describe('is event driven', function() {
         it('should  be inherited from goog.events.EventTarget', function() {
             var model = new tart.components.Model();
             expect(model instanceof goog.events.EventTarget).toBeTruthy();
+        });
+
+
+        it("should supply events to it's sub classes", function() {
+            var subModelClass = function() {
+                goog.base(this);
+            };
+            goog.inherits(subModelClass, tart.components.Model);
+
+            var subModel = new subModelClass();
+
+            var text;
+
+            subModel.addEventListener('foo', function(e) {
+                text = 'foo triggered';
+            });
+
+            subModel.dispatchEvent({type: 'foo'});
+
+            expect(text).toEqual('foo triggered');
         });
     });
 });
