2b09b9b594779b86f6d853beba277e927065b3f9
dashersw <armagan.amcalar@tart.com.tr>
2011-06-27 16:06:49 +0300

    [tartMVC] Added example implementation application

---
+++ tart/mvc/test/application/controllers/GamesController.js
@@ -0,0 +1,56 @@
+// Copyright 2011 Tart. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+goog.provide('mvcapp.controllers.GamesController');
+goog.require('mvcapp.views.scripts.games.index');
+goog.require('mvcapp.views.scripts.games.list');
+goog.require('mvcapp.views.layouts.rare');
+/**
+ * @constructor
+ * @extends {tart.mvc.Controller}
+ */
+mvcapp.controllers.GamesController = function() {
+    goog.base(this);
+};
+goog.inherits(mvcapp.controllers.GamesController, tart.mvc.Controller);
+
+/**
+ * @this {tart.mvc.Action}
+ */
+mvcapp.controllers.GamesController.indexAction = function() {
+    this.setViewScript(mvcapp.views.scripts.games.index);
+    this.setLayout(mvcapp.views.layouts.rare);
+    console.log('games index');
+    console.dir(this);
+    this.view.p1 = this.params['p1'];
+    this.view.p2 = this.params['p2'];
+    this.view.a = this.params['a'];
+    this.view.b = this.params['b'];
+};
+
+/**
+ * @this {tart.mvc.Action}
+ */
+mvcapp.controllers.GamesController.listAction = function() {
+    this.setViewScript(mvcapp.views.scripts.games.list);
+    this.view.vp1 = this.params['param1'];
+    this.view.vp2 = this.params['param2'];
+    console.log('games list');
+};
+
+/**
+ * @this {tart.mvc.Action}
+ */
+mvcapp.controllers.GamesController.showAction = function() {
+    console.log('games show');
+};
---
+++ tart/mvc/test/application/mvcapp.js
@@ -0,0 +1,65 @@
+// Copyright 2011 Tart. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+goog.require('tart.mvc');
+goog.require('mvcapp.controllers.IndexController');
+goog.require('mvcapp.controllers.GamesController');
+goog.require('mvcapp.views.layouts.common');
+goog.provide('mvcapp.Application');
+
+/**
+ * @constructor
+ * @extends {tart.mvc.Application}
+ */
+mvcapp.Application = function() {
+    goog.base(this);
+};
+goog.inherits(mvcapp.Application, tart.mvc.Application);
+
+mvcapp.Application.prototype.basePath_ = '/tart/mvc/test/';
+mvcapp.Application.prototype.defaultRoute_ = new tart.mvc.uri.Route({
+    name: 'default',
+    format: '',
+    controller: mvcapp.controllers.IndexController,
+    action: mvcapp.controllers.IndexController.listAction
+});
+
+mvcapp.Application.prototype.defaultLayout_ = mvcapp.views.layouts.common;
+
+mvcapp.Application.prototype.initRouting = function() {
+    var router = this.getRouter();
+    router.addRoute(new tart.mvc.uri.Route({
+        name: 'homeshow',
+        format: 'home/show/',
+        controller: mvcapp.controllers.GamesController,
+        action: mvcapp.controllers.GamesController.showAction
+    }));
+    router.addRoute(new tart.mvc.uri.Route({
+        name: 'homeList',
+        format: 'home/list/:param1/:param2/',
+        controller: mvcapp.controllers.GamesController,
+        action: mvcapp.controllers.GamesController.listAction
+    }));
+    router.addRoute(new tart.mvc.uri.Route({
+        name: 'homeList',
+        format: 'home/list/',
+        controller: mvcapp.controllers.GamesController,
+        action: mvcapp.controllers.GamesController.listAction
+    }));
+    router.addRoute(new tart.mvc.uri.Route({
+        name: 'home anything',
+        format: 'home/list/:p1/:p2/*',
+        controller: mvcapp.controllers.GamesController,
+        action: mvcapp.controllers.GamesController.indexAction
+    }));
+}
