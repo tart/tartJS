c7b582c712260f7ba8bad63994bbc263093e5423
Osman Yuksel <osman.yuksel@tart.com.tr>
2011-07-11 17:43:04 +0300

    [modelPlugins] Filter, Page and Sorter plugins implemented

---
+++ tart/component/plugin/Pager.js
@@ -0,0 +1,59 @@
+// Copyright 2011 Tart. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview tart.component.plugin.Pager model pager plugin to handle pagination.
+ */
+
+goog.provide('tart.component.plugin.Pager');
+
+goog.require('goog.structs.Map');
+
+
+/**
+ * @param {tart.component.Model} model
+ * @constructor
+ */
+tart.component.plugin.Pager = function (model) {
+    /** @private */
+    this.model_ = model;
+
+    this.pager_ = new goog.structs.Map();
+};
+
+/**
+ * @param {number} pageCount number of pages.
+ */
+tart.component.plugin.Pager.prototype.setTotalPageCount = function (pageCount) {
+    this.pager_.set("pageCount", pageCount);
+    this.model_.params.set('pager_', this.pager_.toObject());
+};
+
+/**
+ * @param {number} offset cursor start point for paging.
+ */
+tart.component.plugin.Pager.prototype.setOffset = function (offset) {
+    this.pager_.set("offset", offset);
+    this.model_.params.set('pager_', this.pager_.toObject());
+};
+
+
+/**
+ *
+ * @param {number} limit item count to fetch.
+ */
+tart.component.plugin.Pager.prototype.setLimit = function (limit) {
+    this.pager_.set("limit", limit);
+    this.model_.params.set('pager_', this.pager_.toObject());
+};
---
+++ tart/component/plugin/Sorter.js
@@ -0,0 +1,42 @@
+// Copyright 2011 Tart. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview tart.component.plugin.Sorter model sorter plugin.
+ */
+
+goog.provide('tart.component.plugin.Sorter');
+
+goog.require('goog.structs.Map');
+
+
+/**
+ * @param {tart.component.Model} model
+ * @constructor
+ */
+tart.component.plugin.Sorter = function (model) {
+    /** @private */
+    this.model_ = model;
+
+    this.sortBy_ = new goog.structs.Map();
+};
+
+/**
+ * @param {string} field field to be sorted.
+ * @param {string} order order by directive, which is asc or desc.
+ */
+tart.component.plugin.Sorter.prototype.addSort = function (field, order) {
+    this.sortBy_.set(field, order);
+    this.model_.params.set('sortBy_', this.sortBy_.toObject());
+};
