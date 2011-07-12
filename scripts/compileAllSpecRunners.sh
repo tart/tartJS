89151a7ef6e62ea7411720d45e24b4547e3c2cfb
Osman Yuksel <osman.yuksel@tart.com.tr>
2011-07-12 17:13:06 +0300

    [compileAllSpecRunners] script to compile all spec runners mofidied for SpecRunner.html instead of compiled.js

--- scripts/compileAllSpecRunners.sh
+++ scripts/compileAllSpecRunners.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-for i in `find * -type f | grep -i spec | grep -i compiled.js`; 
+for i in `find * -type f | grep -i spec | grep -i "SpecRunner.html$" | grep -v "third_party"`; 
 do
-    echo -e "========= COMPILING $i =========" ; 
-    scripts/compileForSpecRunner.sh $i 2> /dev/null; 
+    compiledJs=`echo $i | sed -e 's/SpecRunner.html/compiled.js/'`
+    echo -e "========= COMPILING $compiledJs =========" ; 
+    scripts/compileForSpecRunner.sh $compiledJs 2> /dev/null; 
     echo -e "========= FINISHED $? =========\n"; 
 done
