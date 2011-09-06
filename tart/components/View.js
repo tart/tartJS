cb0e5c44bef257f1236656159d4cfead1c9bdcb8
dashersw <armagan.amcalar@tart.com.tr>
2011-09-06 05:40:43 +0300

    [components] Fixed another JSDoc type declaration in View class's setDOM method.

--- tart/components/View.js
+++ tart/components/View.js
@@ -74,7 +74,7 @@ tart.components.View.prototype.render = function() {
 
 /**
  * Sets base DOM tree for component
- * @param {Object} dom base DOM reference for component.
+ * @param {jQueryObject} dom base DOM reference for component.
  */
 tart.components.View.prototype.setDOM = function(dom) {
     this.$dom_ = dom;
