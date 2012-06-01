3ad8d99d965d72b15d692d5becf048acedcfc672
spina <emre.oztunc@tart.com.tr>
2012-06-01 18:43:18 +0300

    [ui][tooltip] Event Listeners and unlistening lines for tooltips to reposition itself was added. ( related with TACD-84 )

--- tart/ui/tooltip/TooltipComponent.js
+++ tart/ui/tooltip/TooltipComponent.js
@@ -117,6 +117,13 @@ tart.ui.TooltipComponent.prototype.onShow = function() {
     document.body.appendChild(this.element);
     this.position();
     this.element.style.display = 'block';
+
+    this.windowResizeListener = goog.events.listen(window, goog.events.EventType.RESIZE, function(e) {
+        this.position();
+    }, false, this);
+    this.windowScrollListener = goog.events.listen(window, goog.events.EventType.SCROLL, function(e) {
+        this.position();
+    }, false, this);
 };
 
 tart.ui.TooltipComponent.prototype.reset = function() {
@@ -126,6 +133,8 @@ tart.ui.TooltipComponent.prototype.reset = function() {
 tart.ui.TooltipComponent.prototype.onInit = function() {
     this.element.style.display = 'none';
     goog.dom.removeNode(this.cap);
+    goog.events.unlistenByKey(this.windowResizeListener);
+    goog.events.unlistenByKey(this.windowScrollListener);
 };
 
 tart.ui.TooltipComponent.prototype.templates_base = function() {
