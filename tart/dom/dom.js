186f63070677c2587f35c4b0831a0df4bec248cb
dashersw <armagan@amcalar.com>
2012-09-13 00:31:34 +0300

    [dom] Fixed typos on createElement method, the return type is now Node.

--- tart/dom/dom.js
+++ tart/dom/dom.js
@@ -24,14 +24,14 @@ goog.provide('tart.dom');
     var tempDiv = document.createElement('div');
 
     /**
-     * Stripped down version of goog.dom.htmlToDocumentFragment. It's performance is fantastic across all browsers.
+     * Stripped down version of goog.dom.htmlToDocumentFragment. Its performance is fantastic across all browsers.
      *
      * This version won't work with <code><script></code> and <code><style></code> tags in IE.
      * Also, it requires only one element in the top hieararchy, which basically means you have to combine
      * your elements under one parent div, or you will only get the first element.
      *
      * @param {string} htmlString The HTML string to convert.
-     * @return {!Element} The resulting element.
+     * @return {Node} The resulting element.
      */
     tart.dom.createElement = function(htmlString) {
         tempDiv.innerHTML = htmlString;
