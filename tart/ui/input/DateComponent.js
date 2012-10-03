40993f4b6be91aa1630496ca053b8a18a2a0cd14
Emre Oztunc <emre.oztunc@tart.com.tr>
2012-10-03 18:48:08 +0300

    [ui][input][date] Changed the DateComponent's name and updated various fields.

--- tart/ui/input/DateComponent.js
+++ tart/ui/input/DateComponent.js
@@ -11,9 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
-// @author A.Emre Öztunç <emre.oztunc@tart.com.tr>
-
 
 
 
@@ -21,40 +18,31 @@
  * @fileoverview
  */
 
-goog.provide('tart.ui.input.DateInputDlgComponent');
+goog.provide('tart.ui.input.DateComponent');
 goog.require('tart.ui.DlgComponent');
 goog.require('goog.events.EventTarget');
 
 
 /**
- * Date Input component.
+ * Date component.
  * @extends {tart.ui.DlgComponent}
  * @constructor
  */
-tart.ui.input.DateInputDlgComponent = function() {
+tart.ui.input.DateComponent = function() {
     goog.base(this);
 
     this.dateString = '';
     this.formattedDateString = '';
 
 };
-goog.inherits(tart.ui.input.DateInputDlgComponent, tart.ui.DlgComponent);
+goog.inherits(tart.ui.input.DateComponent, tart.ui.DlgComponent);
 
 
 /**
- * DateInputDlgComponent domMappings.
- * @type {Object}
- */
-tart.ui.input.DateInputDlgComponent.prototype.mappings = {
-    INPUT : '.dateInput'
-};
-
-
-/**
- * Returns the date in miliseconds format.
+ * Returns the date in milliseconds format.
  * @return {number}
  */
-tart.ui.input.DateInputDlgComponent.prototype.getDateTime = function() {
+tart.ui.input.DateComponent.prototype.getDateTime = function() {
     var formattedDateString = this.dateString.slice(2, 4) + '/' + this.dateString.slice(0, 2) + '/' +
         this.dateString.slice(4, this.dateString.length);
     var date = new Date(formattedDateString);
@@ -68,7 +56,7 @@ tart.ui.input.DateInputDlgComponent.prototype.getDateTime = function() {
  *
  * @param {goog.events.BrowserEvent} e keyPress Event.
  */
-tart.ui.input.DateInputDlgComponent.prototype.onKeyPress = function(e) {
+tart.ui.input.DateComponent.prototype.onKeyPress = function(e) {
     var dateInputArea = this.getChild('input')[0];
     if(e.keyCode == 8 || e.keyCode == 13 || e.keyCode == 191 || (e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105)) {
         var dateInputAreaText = '';
@@ -89,8 +77,7 @@ tart.ui.input.DateInputDlgComponent.prototype.onKeyPress = function(e) {
         if(dateString.length <= 2) {
             if(dateString.length == 1 && e.keyCode == 191) {
                 dateString = '0' + dateString;
-                dateInputAreaText = dateString.slice(0, dateString.length);
-                dateInputAreaText += '/';
+                dateInputAreaText = dateString.slice(0, dateString.length) + '/';
             }
             else {
                 dateInputAreaText = dateString.slice(0, dateString.length);
@@ -101,24 +88,18 @@ tart.ui.input.DateInputDlgComponent.prototype.onKeyPress = function(e) {
         }
         else {
             if(dateString.length >2 && dateString.length <= 4) {
-                dateInputAreaText = dateString.slice(0, 2);
-                dateInputAreaText += '/';
+                dateInputAreaText = dateString.slice(0, 2) + '/';
                 if(e.keyCode == 191) {
                     if(dateString.length == 3)
                         dateString = dateString.slice(0, 2) + '0' + dateString.slice(2, 3);
-                    dateInputAreaText += dateString.slice(2, 4);
-                    dateInputAreaText += '/';
+                    dateInputAreaText += dateString.slice(2, 4) + '/';
                 }
                 else
                     dateInputAreaText += dateString.slice(2, dateString.length);
                 this.dateString = dateString;
             }
             else if(dateString.length > 4) {
-                dateInputAreaText = dateString.slice(0, 2);
-                dateInputAreaText += '/';
-                dateInputAreaText += dateString.slice(2, 4);
-                dateInputAreaText += '/';
-                dateInputAreaText += dateString.slice(4, dateString.length);
+                dateInputAreaText = dateString.slice(0, 2) + '/' + dateString.slice(2, 4) + '/' + dateString.slice(4, dateString.length);
                 this.dateString = dateString;
             }
         }
@@ -139,7 +120,7 @@ tart.ui.input.DateInputDlgComponent.prototype.onKeyPress = function(e) {
 /**
  * Dispatches an event in case of focusing into the component and cleans the text area if no text entrance was made.
  */
-tart.ui.input.DateInputDlgComponent.prototype.onFocusIn = function(e) {
+tart.ui.input.DateComponent.prototype.onFocusIn = function(e) {
     var dateInputArea = this.getChild('input')[0];
     if(this.dateString == '')
         dateInputArea.value = '';
@@ -149,7 +130,7 @@ tart.ui.input.DateInputDlgComponent.prototype.onFocusIn = function(e) {
 /**
  * Dispatches an event after focusOut action which carries the date in milisecs.
  */
-tart.ui.input.DateInputDlgComponent.prototype.onFocusOut = function(e) {
+tart.ui.input.DateComponent.prototype.onFocusOut = function(e) {
     if(this.dateString == '')
         this.resetInputArea();
 };
@@ -158,7 +139,7 @@ tart.ui.input.DateInputDlgComponent.prototype.onFocusOut = function(e) {
 /**
  * Resets the text area, date string and formatted date string parameters.
  */
-tart.ui.input.DateInputDlgComponent.prototype.resetInputArea = function() {
+tart.ui.input.DateComponent.prototype.resetInputArea = function() {
     this.dateString = '';
     var dateInputArea = this.getChild('input')[0];
     dateInputArea.value = 'GG/AA/YYYY';
@@ -170,7 +151,7 @@ tart.ui.input.DateInputDlgComponent.prototype.resetInputArea = function() {
  * Constructs the base template
  * @return {string} base template
  */
-tart.ui.input.DateInputDlgComponent.prototype.templates_base = function() {
+tart.ui.input.DateComponent.prototype.templates_base = function() {
     return '<span id="' + this.id + '">' +
                 '<input name="dateInput" id="dateInputArea" type="text"' +
                 'class="textForm numberOnly dateInput" minlength="1" maxlength="8" value="GG/AA/YYYY"/>' +
@@ -178,13 +159,22 @@ tart.ui.input.DateInputDlgComponent.prototype.templates_base = function() {
 };
 
 
+/**
+ * DateComponent domMappings.
+ * @type {Object}
+ */
+tart.ui.input.DateComponent.prototype.mappings = {
+    INPUT : '.dateInput'
+};
+
+
 (function() {
-    var proto = tart.ui.input.DateInputDlgComponent.prototype;
+    var proto = tart.ui.input.DateComponent.prototype;
     proto.events = {};
-    var DOMFocusIn = proto.events[goog.events.EventType.FOCUSIN] = {};
-    DOMFocusIn[proto.mappings.INPUT] = proto.onFocusIn;
-    var DOMFocusOut = proto.events[goog.events.EventType.FOCUSOUT] = {};
-    DOMFocusOut[proto.mappings.INPUT] = proto.onFocusOut;
+    var focusIn = proto.events[goog.events.EventType.FOCUSIN] = {};
+    focusIn[proto.mappings.INPUT] = proto.onFocusIn;
+    var focusOut = proto.events[goog.events.EventType.FOCUSOUT] = {};
+    focusOut[proto.mappings.INPUT] = proto.onFocusOut;
     var keyup = proto.events[goog.events.EventType.KEYUP] = {};
     keyup[proto.mappings.INPUT] = proto.onKeyPress;
 })();
